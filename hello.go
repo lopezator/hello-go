1bdb2b0b36dc7e7a154dbdac5e2e337e55c2a4e2
David López <dlopbec@gmail.com>
2017-09-24 19:11:25 +0200

    Some printf and println tests.

--- hello.go
+++ hello.go
@@ -6,4 +6,10 @@ func main() {
     var hello_paco string = "Hello, paco!\n"
     fmt.Printf(hello_paco)
     fmt.Println("1 + 2 =", 3); //Pinta las variables que le pases con un espacio en medio
+    fmt.Println(len("cinco"))
+    var paco = "Paco"
+    fmt.Println(paco + " es un robot")
+    var pi float64 = 3.14159
+    fmt.Printf("%f\n", pi)
+    fmt.Printf("%.2f\n", pi)
 }
