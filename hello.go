6ad411c1ffbc86bd6334a2cdf9bee7935f4afdff
David López <dlopbec@gmail.com>
2017-09-24 17:21:39 +0200

    First commit

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    var hello_paco string = "Hello, paco!\n"
+    fmt.Printf(hello_paco)
+    fmt.Println("1 + 2 =", 3); //Pinta las variables que le pases con un espacio en medio
+}
